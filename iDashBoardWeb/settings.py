929b201b8cc91e5c38eb09d5bc170398cca0d515
littlepig2013 <zhuzichen19960417@gmail.com>
2016-04-01 19:53:40 +0800

    message communication

--- iDashBoardWeb/settings.py
+++ iDashBoardWeb/settings.py
@@ -62,7 +62,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'iDashBoard',
         'USER': 'root',
-        'PASSWORD': 'wuzher1||',
+        'PASSWORD': '2013013297',
         'HOST': '127.0.0.1',
         'PORT': 3306
     }
